54f7e6e8a2673883b4227e66de542fa3b0711516
Julio Fraga <juliocesaradefraga@gmail.com>
2020-08-15 18:30:46 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div>
+
+    <app-header></app-header>
+    <ninjas></ninjas>
+    <app-footer></app-footer>
+
+  </div>
+
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Ninjas from './components/Ninjas.vue'
+
+
+export default {
+
+  components:{
+
+    'app-header': Header,
+    'app-footer': Footer,
+    'ninjas': Ninjas
+
+  },
+
+  data () {
+    return {
+
+      
+      
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+
+
+</style>
---
+++ src/components/Ninjas.vue
@@ -0,0 +1,63 @@
+<template>
+
+    <div id="ninjas">
+        <ul>
+            <li v-for="ninja in ninjas" v-on:click="ninja.show =! ninja.show">
+                <h2>{{ninja.name}}</h2>
+                <h3 v-show="ninja.show">{{ninja.speciality}}</h3>
+            </li>
+        </ul>
+    </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+      ninjas:[
+          {name:'Julio', speciality:'Front-End', show:false},
+          {name:'Vitor', speciality:'Back-End', show:false},
+          {name:'Gabriel Fraga', speciality:'Alguma coisa', show:false},
+          {name:'Gabriel Pioner', speciality:'Outra coisa', show:false},
+          {name:'Mateus Winter', speciality:'Ser guei', show:false},
+          {name:'Pessoa random', speciality:'Nada', show:false},
+      ]
+      
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+#ninjas{
+
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
+
+}
+ul{
+
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    padding: 0;
+}
+li{
+
+    flex-grow: 1;
+    flex-basis: 300px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+    margin: 10px;
+
+}
+
+</style>
